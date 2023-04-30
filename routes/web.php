297d78a6f45cc30f64cbd2074d70d241dc967f47
PatCard <patcard@hotmail.com>
2023-04-30 01:01:19 -0400

    Se modifico web.php para mostrar enrutamiento

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+/**
+ * Route::get()    |  Consultar
+ * Route::post()   |  Guardar
+ * Route::delete() |  Eliminar
+ * Route::put()    |  Actualizar
+ */
+
+ Route::get('/', function () {
+    //return view('welcome');
+    return 'Ruta home';
+});
+
+Route::get('blog', function () {
+    return 'Listado de publicaciones';
+});
+
+Route::get('blog/{slug}', function ($slug) {
+    // consulta en base de datos
+
+    return $slug;
+});
+
+Route::get('buscar', function (Request $request) {
+    return $request->all();
 });
