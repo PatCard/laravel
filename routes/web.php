6bb96f7a04db75c746000e1c98263a9a1deb1154
PatCard <patcard@hotmail.com>
2023-04-30 01:30:01 -0400

    Se cran nuevas rutas clase 5

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,24 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-/**
- * Route::get()    |  Consultar
- * Route::post()   |  Guardar
- * Route::delete() |  Eliminar
- * Route::put()    |  Actualizar
- */
-
- Route::get('/', function () {
-    //return view('welcome');
-    return 'Ruta home';
+Route::get('/', function () {
+    return view('home');
 });
 
 Route::get('blog', function () {
-    return 'Listado de publicaciones';
+    // consulta en base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP',     'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+
+    return view('blog', ['posts' => $posts]);
 });
 
 Route::get('blog/{slug}', function ($slug) {
-    // consulta en base de datos
+    // consulta en base de datos con el slug
+    $post = $slug;
 
-    return $slug;
-});
-
-Route::get('buscar', function (Request $request) {
-    return $request->all();
+    return view('post', ['post' => $post]);
 });
